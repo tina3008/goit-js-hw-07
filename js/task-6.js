978f45391bb0c86b1d2d331e1fcb83db55ba73e4
tina3008 <kicja_3@ukr.net>
2024-02-07 19:44:23 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -12,7 +12,7 @@ const boxes = document.querySelector('#boxes');
 const ctrls = document.querySelector('#controls');
 
 
-// const array = [];
+
 let inptVal
 
 create.addEventListener("click", () => {
@@ -27,34 +27,23 @@ create.addEventListener("click", () => {
 });
 
 
-// const divArray = [];
+
 let boxsize
 let newDiv 
 
 destroy.addEventListener("click", () => {
-  txtInpt.value = '';  
-   const divArray = [];
+  txtInpt.value = '';    
    boxes.innerHTML = "";   
-  //  divArray.length = 0;
   
  });
 
-//  boxes.style.display = "inline-flex";
 
-function createBoxes(amount){
+function createBoxes(amount) {
   boxes.innerHTML = "";
   boxsize = 30;
-
-  for (let i =1; i <= amount; i+=1 ) {   
-    newDiv =`<div style= "width: ${boxsize} px; height: ${boxsize}px; background: ${getRandomHexColor()}; " /></div>`
-  
-    //  newDiv =`<div style= "padding-top: ${boxsize} px; padding-left: ${boxsize}px; background: ${getRandomHexColor()}; " /> </div>`
-    
+  for (let i = 1; i <= amount; i += 1) {
+    newDiv = `<div style="width: ${boxsize}px; height: ${boxsize}px; background: ${getRandomHexColor()}"></div>`;
     boxes.insertAdjacentHTML("beforeend", newDiv);
     boxsize += 10;
-  //  divArray.push(newDiv)
-
-  };
-  // boxes.insertAdjacentHTML("beforeend", divArray.join(''));
-  // divArray.length=0;
+  }
 }
